666470bde668cf9f4655a9e9303191b48a1572f4
hsushshudoart <hsush.han@gmail.com>
2024-03-18 23:36:56 +0800

    更新閃爍秒數，製作blink_function

--- 2024_03_17_pico_w_test.py
+++ 2024_03_17_pico_w_test.py
@@ -4,10 +4,36 @@ import machine
 import time
 from machine import Pin
 
+
+
 led25 = Pin("LED", Pin.OUT)
+counter = 0
+n = 0
+
+def blink_function(t1,t2):
+    led25.value(1)
+    time.sleep(t1)
+    led25.value(0)
+    time.sleep(t2)
+
+    #n += 1
+
+
 while True:
     print(machine.freq(),"MHz")
+    
+    '''
     led25.value(1)
     time.sleep(1)
     led25.value(0)
     time.sleep(1)
+    '''
+    if(n<=3):
+        blink_function(0.5, 0.5)
+        n += 1
+        print(n)
+    
+    else:
+        blink_function(3, 0.5)
+        n += 1
+        print(n)
