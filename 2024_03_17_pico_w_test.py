cb793c7e76e3a841843f184de80adc864c2de289
hsushshudoart <hsush.han@gmail.com>
2024-03-18 10:18:58 +0800

    2024.03.18_picoW_每1秒閃1次

---
+++ 2024_03_17_pico_w_test.py
@@ -0,0 +1,13 @@
+#machine.freq()  # get the current frequency of the CPU
+
+import machine
+import time
+from machine import Pin
+
+led25 = Pin("LED", Pin.OUT)
+while True:
+    print(machine.freq(),"MHz")
+    led25.value(1)
+    time.sleep(1)
+    led25.value(0)
+    time.sleep(1)
